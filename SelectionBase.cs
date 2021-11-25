e4642674fdb8fbbb7f18ddd4157bca8b16a44a37
Aaron Meaney <aaronmeaney@hotmail.com>
2021-11-25 13:33:19 +0000

    Add SelectionBase script

---
+++ SelectionBase.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Reccy.UnityEditorUtil
+{
+    /// <summary>
+    /// Place this on any GameObject that you want to select by default when selecting its children.
+    /// </summary>
+    [SelectionBase]
+    public class SelectionBase : MonoBehaviour { }
+}
